947a46a142aa851bbc6e53a231a60b2ecc9e6c94
Karsten <karstenba@protonmail.com>
2020-08-18 16:27:07 +0200

    add volume button functionality

--- config.h
+++ config.h
@@ -59,6 +59,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockscreen[]  = { "slock", NULL };
+static const char *lowervolume[]  = { "amixer", "set", "Master", "5%-", NULL };
+static const char *raisevolume[]  = { "amixer", "set", "Master", "5%+", NULL };
 
 static Key keys[] = {
 	/* modifier             key    function        argument */
@@ -96,6 +98,8 @@ static Key keys[] = {
 	TAGKEYS(                18,                    8)                 // 9
 	{ MODKEY|ShiftMask,     24,    quit,           {0} },             // q
 	{ MODKEY,               66,    spawn,          {.v = lockscreen } }, // Caps Lock
+	{ 0,               122,   spawn,          {.v = lowervolume } },
+	{ 0,               123,   spawn,          {.v = raisevolume } },
 };
 
 /* button definitions */
